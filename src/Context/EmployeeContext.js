16dad2e1951c9543dd8d99ea6bd74c1312b50e4a
Karine Valenca <valenca.karine@gmail.com>
2020-03-24 23:03:07 -0300

    created reducer to fetch users data

--- src/Context/EmployeeContext.js
+++ src/Context/EmployeeContext.js
@@ -6,21 +6,32 @@ const employeeReducer = ( state, action ) => {
     switch(action.type) {
         case 'add_employee':
             return { ...state }
+        case 'fetch_employee_success':
+            return action.payload
         case 'add_error':
             return { ...state, errorMessage: action.payload, isLoading: false}
-
         case 'default':
             return { ...state }
     }
 }
 
 const createEmployee = (dispatch) => (name, phone, shift) => {
-    console.log(name, phone, shift)
-    dispatch({ type: 'add_employee'})
+    const { currentUser } = firebase.auth()
+    firebase.database().ref(`/users/${currentUser.uid}/employees`).push({ name, phone, shift })
+    dispatch({ type: 'add_employee' })
+    navigate('Employee')
+}
+
+const fetchEmployees = (dispatch) => () => {
+    const { currentUser } = firebase.auth()
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+        .on('value', snapshot => {
+            dispatch({ type: 'fetch_employee_success', payload: snapshot.val()})
+        })
 }
 
 export const {Provider, Context } = createDataContext(
     employeeReducer,
-    { createEmployee },
+    { createEmployee, fetchEmployees },
     { errorMessage: '' } 
 )
--- src/screens/EmployeeScreen.js
+++ src/screens/EmployeeScreen.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useContext, useEffect } from 'react'
+import { FlatList, View, Text, Button } from 'react-native'
 import { NavigationActions } from 'react-navigation'
 import { navigate } from '../navigateRef'
+import { Context } from '../context/EmployeeContext'
 
 const EmployeeScreen = ({ navigation }) => {
+    const { state, fetchEmployees } = useContext(Context)
+
+    useEffect(() => {
+        console.log("CARREGANDO LISTA")
+        fetchEmployees()
+        console.log(state)
+    }, [])
 
     return (
         <View>
-            <Text>EmployeeScreen</Text>
+           <FlatList
+				data={state}
+				renderItem={({ item }) => {
+                    return(
+                        <Text>Hello</Text>
+                    )
+                }}
+				keyExtractor={(employee, index) => index.toString()}
+			/>
         </View>
     )
 }
